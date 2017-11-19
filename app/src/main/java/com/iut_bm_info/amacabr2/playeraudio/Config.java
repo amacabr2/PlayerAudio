9a6b4f3bdee34dbc74253b5ad81bcc0d71265378
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-11-19 19:32:18 +0100

    Init projet + configuration

---
+++ app/src/main/java/com/iut_bm_info/amacabr2/playeraudio/Config.java
@@ -0,0 +1,10 @@
+package com.iut_bm_info.amacabr2.playeraudio;
+
+/**
+ * Created by amacabr2 on 19/11/17.
+ */
+
+public class Config {
+
+    public static final String CLIENT_ID = "b3e58eefda17605c8215157fdc46266e";
+}

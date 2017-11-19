f772c6dadfc78560ca18c6ceced8ba6298e77cee
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-11-19 19:39:38 +0100

    Singleton pour la librairie Volley

---
+++ app/src/main/java/com/iut_bm_info/amacabr2/playeraudio/VolleySingleton.java
@@ -0,0 +1,38 @@
+package com.iut_bm_info.amacabr2.playeraudio;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by amacabr2 on 19/11/17.
+ */
+
+public class VolleySingleton {
+
+    private static VolleySingleton volleySingleton;
+
+    private static Context context;
+
+    private RequestQueue requestQueue;
+
+    private VolleySingleton(Context ctx) {
+        context = ctx;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context ctx) {
+        if (volleySingleton == null) {
+            volleySingleton = new VolleySingleton(ctx);
+        }
+        return volleySingleton;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+}

830830b632849df6df413c3e20338d7aa6e55ab8
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-11-23 17:17:29 +0100

    Recycler view

--- app/src/main/java/com/iut_bm_info/amacabr2/playeraudio/activities/MainActivity.java
+++ app/src/main/java/com/iut_bm_info/amacabr2/playeraudio/activities/MainActivity.java
@@ -2,23 +2,44 @@ package com.iut_bm_info.amacabr2.playeraudio.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.iut_bm_info.amacabr2.playeraudio.R;
+import com.iut_bm_info.amacabr2.playeraudio.adapters.SongAdapter;
 import com.iut_bm_info.amacabr2.playeraudio.api_connect.SoundCloudApiRequest;
 import com.iut_bm_info.amacabr2.playeraudio.api_connect.VolleySingleton;
 import com.iut_bm_info.amacabr2.playeraudio.models.Song;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SongAdapter.RecyclerItemClickListener {
+
+    private RecyclerView recycler;
+
+    private SongAdapter adapter;
+
+    private List<Song> songs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeViews();
         setContentView(R.layout.activity_main);
         getSongs();
+
+        songs = new ArrayList<>();
+
+        recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        adapter = new SongAdapter(getApplicationContext(), songs, this);
+        recycler.setAdapter(adapter);
+    }
+
+    public void initializeViews() {
+        recycler = findViewById(R.id.mainActivity_recycler);
     }
 
     public void getSongs() {
@@ -27,8 +48,9 @@ public class MainActivity extends AppCompatActivity {
 
         request.getSongs(new SoundCloudApiRequest.SoundCloudInterface() {
             @Override
-            public void onSuccess(List<Song> songs) {
-
+            public void onSuccess(List<Song> newSongs) {
+                songs.addAll(newSongs);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -36,5 +58,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onClickListener(Song song, int position) {
+        Toast.makeText(this, song.getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/iut_bm_info/amacabr2/playeraudio/adapters/SongAdapter.java
@@ -0,0 +1,90 @@
+package com.iut_bm_info.amacabr2.playeraudio.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.iut_bm_info.amacabr2.playeraudio.R;
+import com.iut_bm_info.amacabr2.playeraudio.models.Song;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by amacabr2 on 23/11/17.
+ */
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+
+    private Context context;
+
+    private List<Song> songs;
+
+    private RecyclerItemClickListener listener;
+
+    public SongAdapter(Context context, List<Song> songs, RecyclerItemClickListener listener) {
+        this.context = context;
+        this.songs = songs;
+        this.listener = listener;
+    }
+
+    @Override
+    public SongViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_row, parent, false);
+        return new SongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SongViewHolder holder, int position) {
+        Song song = songs.get(position);
+
+        if (song != null) {
+            holder.tvTitle.setText(song.getTitle());
+            holder.tvArtist.setText(song.getArtist());
+            holder.tvDuration.setText(String.valueOf(song.getDuration()));
+            Picasso.with(context).load(song.getArtworkUrl()).placeholder(R.drawable.music_placeholder).into(holder.ivArtWork);
+
+            holder.bind(song, listener);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public static class SongViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvTitle, tvArtist, tvDuration;
+
+        private ImageView ivArtWork, icPlayActive;
+
+        public SongViewHolder(View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.songRow_title);
+            tvArtist = itemView.findViewById(R.id.songRow_artiste);
+            tvDuration = itemView.findViewById(R.id.songRow_duration);
+            ivArtWork = itemView.findViewById(R.id.songRow_image);
+            icPlayActive = itemView.findViewById(R.id.songRow_playActive);
+        }
+
+        public void bind(final Song song, final RecyclerItemClickListener listener) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onClickListener(song, getLayoutPosition());
+                }
+            });
+        }
+    }
+
+    public interface RecyclerItemClickListener {
+
+        void  onClickListener(Song song, int position);
+    }
+}
